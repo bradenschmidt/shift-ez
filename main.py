096757966ad7bba6dc0ad8a5802aab354f7f7770
Braden Schmidt <bradenschmidt@gmail.com>
2015-06-04 22:15:30 -0600

    Add basic application.

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import urllib
+
+from flask import Flask, url_for
+
+import jinja2
+import webapp2
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+@app.route('/')
+def homepage():
+    user = users.get_current_user()
+    if user:
+        url = users.create_logout_url('')
+        url_linktext = 'Logout'
+    else:
+        url = users.create_login_url('')
+        url_linktext = 'Login'
+
+    filename = "images/schedule/s1.jpg"
+
+    template_values = {
+        'dep': "Lumber",
+        'week': 1,
+        'user': user,
+        'filename': url_for('static', filename=filename)
+    }
+
+    template = JINJA_ENVIRONMENT.get_template('index.html')
+    return template.render(template_values)
+
+
+@app.route('/hello')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
