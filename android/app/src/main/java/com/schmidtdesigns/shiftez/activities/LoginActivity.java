cb1c884c850fff0a448864defdb3356fc2aac945
Braden Schmidt <bradenschmidt@gmail.com>
2015-07-15 00:25:59 -0600

    Remove login and profile setup fragments. Move login setup into activity.

--- android/app/src/main/java/com/schmidtdesigns/shiftez/activities/LoginActivity.java
+++ android/app/src/main/java/com/schmidtdesigns/shiftez/activities/LoginActivity.java
@@ -1,20 +1,32 @@
 package com.schmidtdesigns.shiftez.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.plus.Plus;
 import com.google.android.gms.plus.model.people.Person;
+import com.octo.android.robospice.persistence.DurationInMillis;
+import com.octo.android.robospice.persistence.exception.SpiceException;
+import com.octo.android.robospice.request.listener.RequestListener;
+import com.schmidtdesigns.shiftez.Constants;
 import com.schmidtdesigns.shiftez.R;
-import com.schmidtdesigns.shiftez.fragments.LoginFragment;
-import com.schmidtdesigns.shiftez.fragments.ProfileSetupFragment;
+import com.schmidtdesigns.shiftez.ShiftEZ;
+import com.schmidtdesigns.shiftez.models.Account;
+import com.schmidtdesigns.shiftez.models.PostAccount;
+import com.schmidtdesigns.shiftez.network.AddAccountRetrofitRequest;
+
+import java.util.HashMap;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * Schedule Fragment used to get the schedules from the server, show them in a pager, and allow
@@ -27,6 +39,10 @@ public class LoginActivity extends GPlusBaseActivity {
 
     @InjectView(R.id.toolbar)
     Toolbar mToolbar;
+    @InjectView(R.id.sign_in_button)
+    SignInButton mSignInButton;
+    @InjectView(R.id.progress)
+    ProgressBar mProgress;
 
     public LoginActivity() {
     }
@@ -41,12 +57,30 @@ public class LoginActivity extends GPlusBaseActivity {
             setSupportActionBar(mToolbar);
         }
 
-        displayView(LoginFragment.newInstance());
     }
 
     @Override
     protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
         super.onActivityResult(requestCode, responseCode, intent);
+    }
+
+    @OnClick(R.id.sign_in_button)
+    public void loginWithGPlus() {
+        updateUI(true);
+        if (!login()) {
+            updateUI(false);
+        }
+    }
+
+    // TODO HANDLE FAILED LOGINS UPDATED UI
+    public void updateUI(boolean result) {
+        if (result) {
+            mProgress.setVisibility(View.VISIBLE);
+            mSignInButton.setEnabled(false);
+        } else {
+            mProgress.setVisibility(View.GONE);
+            mSignInButton.setEnabled(true);
+        }
     }
 
     public void getProfileInformation() {
@@ -55,32 +89,48 @@ public class LoginActivity extends GPlusBaseActivity {
 
         // TODO PERSON CAN BE NULL??
         if (person != null) {
-            displayView(ProfileSetupFragment.newInstance(email, person.getDisplayName()));
+            HashMap<String, String> accountParams = new HashMap<>();
+            accountParams.put("user_id", email);
+            accountParams.put("user_name", person.getDisplayName());
+
+            AddAccountRetrofitRequest accountRequest = new AddAccountRetrofitRequest(accountParams);
+            getSpiceManager().execute(accountRequest,
+                    Constants.ADD_ACCOUNT_PARAM,
+                    5 * DurationInMillis.ONE_MINUTE,
+                    new AddAccountRequestListener());
+
         } else {
             Log.e(TAG, "Person was null.");
-            //updateUI(true);
-            loginFailedUI();
+            updateUI(false);
         }
     }
 
-    private void loginFailedUI() {
-        LoginFragment fragment = (LoginFragment) getSupportFragmentManager().
-                findFragmentById(R.id.fragment_container);
-        fragment.updateUI(false);
-    }
-
-    public void displayView(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-        // Replace whatever is in the fragment_container view with this fragment,
-        // and add the transaction to the back stack if needed
-        transaction.replace(R.id.fragment_container, fragment);
-        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
-            transaction.addToBackStack(null);
+    private class AddAccountRequestListener implements RequestListener<PostAccount> {
+        @Override
+        public void onRequestFailure(SpiceException spiceException) {
+            Log.e(TAG, spiceException.getLocalizedMessage());
         }
 
-        // Commit the transaction
-        transaction.commit();
+        @Override
+        public void onRequestSuccess(PostAccount postAccount) {
+            Log.i(TAG, "ACCOUNT: " + postAccount);
+            saveAccount(postAccount.getAccount());
+        }
     }
 
+    public void saveAccount(Account account) {
+        ShiftEZ.getInstance().setAccount(account);
+
+        // get shared preferences
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        //get the preferences editor
+        SharedPreferences.Editor editor = pref.edit();
+
+        // save user info
+        editor.putString(Constants.ACCOUNT_PARAM, Account.serializeToJson(ShiftEZ.getInstance().getAccount()));
+        editor.apply();
+
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
 }
