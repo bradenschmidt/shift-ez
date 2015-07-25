828fab6b7f8a5ac69569da7e58cc0c7f341ed43c
Braden Schmidt <bradenschmidt@gmail.com>
2015-07-25 09:51:07 -0600

    Still more missed.

---
+++ app_engine/app/utils/errors.py
@@ -0,0 +1,10 @@
+class Errors:
+    def __init__(self):
+        pass
+
+    account_not_found = 100
+    key_not_found = 201
+    key_too_old = 202
+    store_not_found = 300
+    store_in_account = 101
+    store_already_exists = 102
