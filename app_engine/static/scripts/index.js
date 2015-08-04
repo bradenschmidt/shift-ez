9e686e1b75185839f71036d465c3ce51599c7f26
Braden Schmidt <bradenschmidt@gmail.com>
2015-08-04 12:14:51 -0600

    Show only one schedule. Add prev and next buttons.

--- app_engine/static/scripts/index.js
+++ app_engine/static/scripts/index.js
@@ -1,10 +1,13 @@
 function hideAllSchedules() {
-    $(".schedules").hide()
+    $(".schedules").hide();
+    showSchedule(0);
 }
 
 function initializeFunction() {
+    $(".schedule").hide();
     hideAllSchedules();
     showStoreSchedulesByIndex(0);
+    showSchedule(null, 0);
 }
 
 $(document).ready(initializeFunction);
@@ -14,13 +17,59 @@ $(".store").click(function () {
 
     var store = $(this).attr('id');
 
-    var storeNum = store.substr(5);
+    if (store !== undefined) {
+        var storeNum = store.substr(5);
 
-    showStoreSchedulesByIndex(storeNum)
+        showStoreSchedulesByIndex(storeNum)
+    }
+
 });
 
 function showStoreSchedulesByIndex(storeNum) {
-    var scheduleNum = '#schedules' + storeNum;
+    var scheduleNum = '.schedules' + storeNum;
+
+    $("#wPaginate").wPaginate('total', 5);
 
     $(scheduleNum).show()
+}
+
+$("#nextButton").click(function () {
+    changeSchedule(1);
+});
+$("#prevButton").click(function () {
+    changeSchedule(-1);
+});
+
+function changeSchedule(index) {
+    console.log("next");
+    var currentSchedule = $("#current");
+
+    var scheduleClass = currentSchedule.attr('class').split(/\s+/)[1];
+
+    if (scheduleClass !== undefined) {
+        var scheduleNum = parseInt(scheduleClass.substr(8));
+        console.log(scheduleNum);
+
+        if (scheduleNum + index >= 0) {
+            showSchedule(currentSchedule, scheduleNum + index);
+        }
+    }
+}
+
+function showSchedule(prev, i) {
+    var scheduleNum = '.schedule' + i;
+
+    if ($(scheduleNum).length) {
+        $(".schedule").hide();
+
+        console.log();
+
+        $(scheduleNum).show();
+        $(scheduleNum).attr('id', 'current');
+        if (prev !== null) {
+            prev.attr('id', '');
+        }
+    }
+
+
 }
